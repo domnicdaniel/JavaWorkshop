2bbbdd89b2e171fdb92d85981f6321503fbb42fd
DOMNIC DANIEL <domnicdaniel871@gmail.com>
2025-12-19 22:12:38 +0530

    Arithmetic Program

---
+++ Arithmetic.java
@@ -0,0 +1,10 @@
+class Hello {
+    public static void main(String[] args)
+    {
+        int a=6;
+        int b=7;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+    }
+}
